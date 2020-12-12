f585ca0871cc5556bd9bd73b2fdfd98dd93129f8
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-11 21:37:14 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -5,14 +5,15 @@ from flask import Flask, render_template, request
 oz_app = Flask(__name__)
 
 @oz_app.route('/home/')
-        def home_view:
-        return render_template('input_selections.html')
+def home_view:
+    return render_template('home.html')
         
 #@oz_app.route('/result/', methods=['GET','POST'])
 #def index():
  #   if request.method == 'POST':
  #       options_selected = request.form.get('regressor')
  #       print(options_selected)
+ #       return render_template('input_selections.html') 
 
 if name == "__main__":
   oz_app.run(debug=TRUE)
