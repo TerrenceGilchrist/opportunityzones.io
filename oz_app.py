031ca420a7f43e64f545d9185c02fa9201e14eb0
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-11 21:52:13 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -1,7 +1,7 @@
 # Structure for the page views
 # December 11, 2020 ~5:51 p.m. to 
 
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
 oz_app = Flask(__name__)
 
 @oz_app.route('/home/')
