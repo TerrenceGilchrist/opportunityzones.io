dfa202900f1b740bcdb0c62097e43e683217e0f6
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-11 19:00:24 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -1,2 +1,18 @@
 # Structure for the page views
-# December 11, 202 ~5:51 p.m. to 
+# December 11, 2020 ~5:51 p.m. to 
+
+from flask import Flask, render_template, request
+oz_app = Flask(__name__)
+
+@oz_app.route('/home/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        options_selected = request.form.get('regressor')
+        print(options_selected)
+        render_template('input_selections.html')
+        
+#@oz_app.route('/result/')
+
+
+if name == "__main__":
+  oz_app.run()
