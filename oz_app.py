5c2c6e62635bac242d9780bcdec316b06a20000a
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-14 15:13:00 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -33,11 +33,33 @@ Test_Tract_Demographics = {
 'Minority Population': [1090,1090,1090],
 'Owner Occupied Units': [231,231,231]
 }
+
+Tract_Demographics_c = {'Year': [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017],
+'Median Family Income Pct': [48,48,45.13,45.4,45.4,45.4,45.4,45.4,45.4,45.4,45.4,38.85,38.85,39.7,39.7,39.7,45.24],
+'Median Family Income': [28752,30432,28793,27512,28988,29238,29147,29646,31144,31144,30236,26224,26379,27552,28187,27433,33432], 
+'Minority Population Pct': [89.58,89.58,90.44,90.44,90.44,90.44,90.44,90.44,90.44,90.44,90.44,84.26,84.26,84.26,84.26,84.26,77.09],
+'Minority Population': [1848,1848,1561,1561,1561,1561,1561,1561,1561,1561,1561,1215,1215,1215,1215,1215,1090],
+'Owner Occupied Units': [319,319,215,215,215,215,215,215,215,215,215,199,199,199,199,199,231]
+}
+
+Test_Tract_Demographics_c
+'Year': [2018,2019,2020],
+'Median Family Income Pct': [45.24,45.24,45.24],
+'Median Family Income': [34382,35242,38273],
+'Minority Population Pct': [77.09,77.09,77.09],
+'Minority Population': [1090,1090,1090],
+'Owner Occupied Units': [231,231,231],
+}
     
 Demographics = pd.DataFrame(data=Tract_Demographics, columns=['Year','Median Family Income Pct','Median Family Income','Minority Population Pct','Minority Population','Owner Occupied Units'])
 Test_Demographics = pd.DataFrame(data=Test_Tract_Demographics, columns=['Year','Median Family Income Pct','Median Family Income','Minority Population Pct','Minority Population','Owner Occupied Units'])
 
+Demographicsc = pd.DataFrame(data=Tract_Demographicsc, columns=['Year','Median Family Income Pct','Median Family Income','Minority Population Pct','Minority Population','Owner Occupied Units'])
+Test_Demographicsc = pd.DataFrame(data=Test_Tract_Demographicsc, columns=['Year','Median Family Income Pct','Median Family Income','Minority Population Pct','Minority Population','Owner Occupied Units'])
+
+
 y=Demographics['Median Family Income']
+yc=Demographicsc['Median Family Income']
 
 @oppzones_app.route('/')
 def home_view():
@@ -56,10 +78,17 @@ def index():
         predicted_income = regressing.predict(X_test)
         predicted_income = predicted_income[0]
         intercept = regressing.intercept_
-        #intercept = round(intercept,2)
         coefficient = regressing.coef_
+        Xc=Demographicsc[options_list]
+        X_testc=Test_Demographicsc[options_list]        
+        regressing.fit(Xc,yc)
+        predicted_incomec = regressing.predict(X_testc)
+        predicted_incomec = predicted_incomec[0]
+        interceptc = regressing.intercept_
+        coefficientc = regressing.coef_
+        #intercept = round(intercept,2)
         #coefficient = round(coefficient,3)
-        return render_template('input_selections.html', options_list=options_list, intercept=intercept, coefficient=coefficient, predicted_income=predicted_income)
+        return render_template('input_selections.html', options_list=options_list, intercept=intercept, coefficient=coefficient, predicted_income=predicted_income, interceptc=interceptc, coefficientc=coefficientc, predicted_incomec=predicted_incomec)
 
 
 if __name__ == "__main__":
