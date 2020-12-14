05265e864afc69ed7c2fa296d0adc32a9217fd26
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-14 12:30:29 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -37,8 +37,8 @@ def home_view():
 def index():
     if request.method == 'POST':
         options_selected = request.form.to_dict(flat=True)
-        options_list=options_selected
-        X=options_selected
+        options_list=list(options_selected)
+        X=Demographics[options_list]
         return render_template('input_selections.html', options_selected=options_selected, options_list=options_list, intercept=intercept, y=y, X=X)
 
 
