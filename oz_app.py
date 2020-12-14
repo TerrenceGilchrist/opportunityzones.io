febf042553152be7d8346ad25c767627db65a787
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-13 19:21:47 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -1,7 +1,7 @@
 # Structure for the page views
 # December 11, 2020 ~5:51 p.m. to 
 
-#import pandas as pd
+import pandas as pd
 #import altair as alt
 
 #from sklearn.linear_model import LinearRegression
@@ -13,15 +13,15 @@ options_list = {}
 chart_list = {}
 
 Tract_Demographics = {
-'Year':[2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001],
-'Median Family Income Pct':[45.24,45.24,45.24,45.24,39.7,39.7,39.7,38.85,38.85,45.4,45.4,45.4,45.4,45.4,45.4,45.4,45.4,45.13,48,48],
-'Median Family Income':[38273,35242,34382,33432,27433,28187,27552,26379,26224,30236,31144,31144,29646,29147,29238,28988,27512,28793,30432,28752],
-'Minority Population Pct':[77.09,77.09,77.09,77.09,84.26,84.26,84.26,84.26,84.26,90.44,90.44,90.44,90.44,90.44,90.44,90.44,90.44,90.44,89.58,89.58],
-'Minority Population':[1090,1090,1090,1090,1215,1215,1215,1215,1215,1561,1561,1561,1561,1561,1561,1561,1561,1561,1848,1848],
-'Owner Occupied Units':[231,231,231,231,199,199,199,199,199,215,215,215,215,215,215,215,215,215,319,319]
+'Year': [2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001],
+'Median Family Income Pct': [45.24,45.24,45.24,45.24,39.7,39.7,39.7,38.85,38.85,45.4,45.4,45.4,45.4,45.4,45.4,45.4,45.4,45.13,48,48],
+'Median Family Income': [38273,35242,34382,33432,27433,28187,27552,26379,26224,30236,31144,31144,29646,29147,29238,28988,27512,28793,30432,28752],
+'Minority Population Pct': [77.09,77.09,77.09,77.09,84.26,84.26,84.26,84.26,84.26,90.44,90.44,90.44,90.44,90.44,90.44,90.44,90.44,90.44,89.58,89.58],
+'Minority Population': [1090,1090,1090,1090,1215,1215,1215,1215,1215,1561,1561,1561,1561,1561,1561,1561,1561,1561,1848,1848],
+'Owner Occupied Units': [231,231,231,231,199,199,199,199,199,215,215,215,215,215,215,215,215,215,319,319]
 }
 
-Demographics = Tract_Demographics
+Demographics = pd.DataFrame(data=Tract_Demographics, columns=['Year','Median Family Income Pct','Median Family Income','Minority Population Pct','Minority Population','Owner Occupied Units'])
 
 @oppzones_app.route('/')
 def home_view():
@@ -36,7 +36,7 @@ def index():
         #print(options_selected)
 	
 	y=Demographics['Median Family Income']
-	X=options_list
+	X=Demographics[options_list]
 	#regressing = linear_model.LinearRegression()
 	#regressing.fit(X,y)
 	#intercept = regressing.intercept_
