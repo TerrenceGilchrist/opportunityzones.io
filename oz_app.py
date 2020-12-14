e7fbb490c9366e2d9183555d57f9a252bb24e1db
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-14 09:31:46 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -41,7 +41,7 @@ def index():
         options_selected = request.form.to_dict(flat=False)
         options_list = list(options_selected.values())
 	y={[38273,35242,34382,33432,27433,28187,27552,26379,26224,30236]}
-	X={[77.09,77.09,77.09,77.09,84.26,84.26,84.26,84.26,84.26,90.44]}
+	X=options_list
 	#y=Demographics['Median Family Income']
 	#X=Demographics[['Median Family Income Pct','Minority Population Pct']]
 	#regressing = LinearRegression()
