5b900f93621564d12558b577adf43f503b025da3
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-15 18:19:36 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -1,5 +1,5 @@
 # Structure for the page views
-# December 11, 2020 ~5:51 p.m. to December 15, 2020 ~5:01 p.m.
+# December 11, 2020 ~5:51 p.m. to December 15, 2020 ~6:19 p.m.
 
 
 from sklearn.linear_model import LinearRegression
