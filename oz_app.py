eefa66d22b82a58c95decb91d49783eac30924cc
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-14 21:26:14 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -62,8 +62,7 @@ def index():
         chart_mp = alt.Chart(Demographics).mark_line().encode(
     		x='Year',
 	    	y='Minority Population'
-        )
-        created_chart = chart_mp.to_json()        
+        )      
         return render_template('input_selections.html', options_list=options_list, intercept=intercept, coefficient=coefficient, predicted_income=predicted_income)
 
 
