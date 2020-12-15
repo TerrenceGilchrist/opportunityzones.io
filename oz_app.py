6165a369d2c22b78d183558cd3b91a0843fa6133
Terrence E. Gilchrist <47313628+TerrenceGilchrist@users.noreply.github.com>
2020-12-15 00:10:11 -0500

    Update oz_app.py

--- oz_app.py
+++ oz_app.py
@@ -3,7 +3,7 @@
 
 
 from sklearn.linear_model import LinearRegression
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import altair as alt
 import pandas as pd
 
